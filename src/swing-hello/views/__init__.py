448ed92e0dfe8ee0f9553a103def0e272664370f
Lars van Vianen <lars@vianen.com>
2024-06-29 10:24:12 +0200

    Create __init__.py

---
+++ src/swing-hello/views/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Views Module
+============
+
+"""
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Local Modules
+from .view_hello_response import hello_response_view
+from .view_hello_response import HelloResponseView
+from .view_hello_template import hello_template_view
+from .view_hello_template import HelloTemplateView
