fec927573e90907d07181c99590695807cdf1ab6
Lars van Vianen <lars@vianen.com>
2024-08-08 19:07:28 +0200

    Update __init__.py

--- src/swing_hello/__init__.py
+++ src/swing_hello/__init__.py
@@ -22,3 +22,15 @@ Swing Hello Module
 # =============================================================================
 # Module Exports
 # =============================================================================
+
+
+
+
+__author__      = "Lars van Vianen"
+__copyright__   = "Copyright (c) 2020 Scape Agency"
+__credits__     = ["Lars van Vianen",]
+__license__     = "Propierty"
+__version__     = "1.0.1"
+__maintainer__  = "Lars van Vianen"
+__email__       = "lars@scape.agency"
+__status__      = "Alpha"
