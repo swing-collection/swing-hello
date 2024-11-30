3be45994a734cba4f41e76738204002f4fc640c6
Lars van Vianen <lars@vianen.com>
2024-11-30 17:55:54 +0100

    Update urls.py

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,8 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+
+"""
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Standard Library
+import os
+
+# Import | Libraries
 from django.contrib import admin
 from django.urls import path, include
 
+# Import | Local Modules
+
+
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin site URL
-    path('hello/', include('swing_hello.urls')),  # Include the URLs from the swing_hello app
-    path('', include('swing_hello.urls')),  # Include the URLs from the swing_hello app
+    path("admin/", admin.site.urls),  # Admin site URL
+    path("hello/", include("swing_hello.urls")),  # Include the URLs from the swing_hello app
+    path("", include("swing_hello.urls")),  # Include the URLs from the swing_hello app
 ]
