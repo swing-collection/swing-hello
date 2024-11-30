c8a4c246a54facaee39d48fad3258c9e628d7437
Lars van Vianen <lars@vianen.com>
2024-11-30 13:51:48 +0100

    Update __init__.py

---
+++ demo/demo/__init__.py
@@ -0,0 +1,6 @@
+"""
+Demo project package for testing the swing_hello reusable app.
+
+This package contains the settings, WSGI configuration, and other
+project-level components for the demo Django project.
+"""
