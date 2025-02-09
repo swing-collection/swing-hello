4f2bbe814a71e9cd9eb44adb747161809bf1c121
Lars van Vianen <lars@vianen.com>
2025-02-09 11:49:08 +0100

    Update apps.py

--- src/swing_hello/apps.py
+++ src/swing_hello/apps.py
@@ -25,21 +25,21 @@ the Django project. It specifies various attributes and settings for the app.
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 # =============================================================================
 # Classes
 # =============================================================================
+
 
 class HelloConfig(AppConfig):
     """
     Hello Config Class
     =================
-    
+
     Configuration class for the 'hello' application.
 
     """
 
     name = "swing_hello"
     label = "swing_hello"
-    verbose_name = _("Swing Hello")
-    default_auto_field = "django.db.models.BigAutoField"
+    verbose_name: str = _(message="Swing Hello")
+    default_auto_field: str = "django.db.models.BigAutoField"
